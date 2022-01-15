60d6661c9f53bbe54abebcfeef16f4baa78cc602
Kartiki02 <59330926+Kartiki02@users.noreply.github.com>
2022-01-15 15:13:17 +0530

    Add files via upload

---
+++ formhandler.php
@@ -0,0 +1,16 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$message = $_POST['message'];
+
+$email_from = 'karitikigangthade@gmail.com';
+$email_subject = 'New form submission';
+$email_body = "User name: $name.\n".
+                "User email: $.visitor_email.\n".
+                "Message: $message.\n";
+$to = "kartikigangthade@gmail.com"
+$headers = "From: $email_from \r\n";
+$headers .="Reply-To:visitor_email \r\n";
+mail($to,$email_subject,$email_body,$headers);
+header("Location: contact.html");
+?>
